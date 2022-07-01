e2920167b8a1c61ed31a83d2b08e4c721efb8722
newfly101-jhkim <jhkim@onthelive.kr>
2022-07-01 16:09:18 +0900

    015. 할일 추가시 입력값 확인

--- web/src/Components/TodoInsert.js
+++ web/src/Components/TodoInsert.js
@@ -1,11 +1,22 @@
 import {MdAdd} from 'react-icons/md';
 import './TodoInsert.scss';
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 
 const TodoInsert = () => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+        console.log(e.target.value); // onChange() 값 console에서 확인
+    }, []);
+
     return (
         <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요"/>
+            <input placeholder="할 일을 입력하세요"
+            value={value}
+            onChange={onChange}
+            />
+
             <button type="submit"> <MdAdd /> </button>
         </form>
     );
